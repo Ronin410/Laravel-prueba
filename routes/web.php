cad4d5bd5c821b60ed3e76c0af2da1e2cf7e5cd4
Alejandro Bueno Mendoza <alejandrobuenomendoza@gmail.com>
2024-10-17 15:52:46 -0700

    cambio a api rest

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/ryan', function () {
     return "wdwq";
+});
+
+
+Route::get('/ryan/{post}', function ($post) {
+    return "wdwq {$post}";
 });
